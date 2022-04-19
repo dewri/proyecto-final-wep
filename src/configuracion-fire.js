29f8f98c4ed7a8eec0b519e41f1e17814aab3416
dewri <95944459+dewri@users.noreply.github.com>
2022-04-18 20:52:20 -0400

    Update configuracion-fire.js

--- src/configuracion-fire.js
+++ src/configuracion-fire.js
@@ -10,13 +10,8 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBij8QcyV3PWU4BifKSd8YiZrWeTBYsNaE",
-  authDomain: "proyecto-final-blog.firebaseapp.com",
-  projectId: "proyecto-final-blog",
-  storageBucket: "proyecto-final-blog.appspot.com",
-  messagingSenderId: "776359044211",
-  appId: "1:776359044211:web:d388395d6dc07089d47606",
-  measurementId: "G-YQJMEN0L5T"
+  //Esta parte la borre porque la funcion de env me estaba dando problemas 
+  //en el programa y no supe como ocultarla 
 };
 
 // Initialize Firebase
